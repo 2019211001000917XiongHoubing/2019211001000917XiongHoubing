8ce748609634770022a7c1e24f394cf01b654b7f
2019211001000917XiongHoubing <77086458+2019211001000917XiongHoubing@users.noreply.github.com>
2021-03-30 20:48:38 +0800

    HW-week4 Commit

--- src/main/java/com/XiongHoubing/week3/demo/RegisterServlet.java
+++ src/main/java/com/XiongHoubing/week3/demo/RegisterServlet.java
@@ -1,33 +1,77 @@
 package com.XiongHoubing.week3.demo;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.*;
 
+@WebServlet(
+        urlPatterns = {"/register"}
+)
 public class RegisterServlet extends HttpServlet {
+    Connection con=null;
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public  void init() {
+        ServletContext config=getServletContext();
+        String driver=config.getInitParameter("driver");
+        String url=config.getInitParameter("url");
+        String username=config.getInitParameter("username");
+        String password=config.getInitParameter("password");
+
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, username, password);
+            System.out.println("init()-->"+con);
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
 
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
 
-        String Username = request.getParameter("Username");
-        String password = request.getParameter("password");
-        String Email = request.getParameter("Email");
-        String Gender = request.getParameter("Gender");
-        String birthdate = request.getParameter("birthdate");
+    }
 
-        PrintWriter writer = response.getWriter();
-        writer.println("<br>Username :"+Username);
-        writer.println("<br>password :"+password);
-        writer.println("<br>Email :"+Email);
-        writer.println("<br>Gender :"+Gender);
-        writer.println("<br>birth Date :"+birthdate);
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        int id= Integer.parseInt(request.getParameter("id"));
+        String username=request.getParameter("username");
+        String password=request.getParameter("password");
+        String email=request.getParameter("email");
+        String gender=request.getParameter("gender");
+        String birthdate= request.getParameter("birthdate");
 
-        writer.close();
+        String sql="insert into usertable(id,username,password,email,gender,birthdate) values("+id+",'"+username+"','"+password+"','"+email+"','"+gender+"','"+birthdate+"')";
+        try {
+            con.createStatement().executeUpdate(sql);
+            ResultSet rs=con.createStatement().executeQuery("select * from usertable");
+            response.setContentType("textml");
+            PrintWriter out= response.getWriter();
+            out.println("<html><body><table border='1'>");
+            out.println("<tr><th>id</th><th>username</th><th>password</th><th>email</th><th>gender</th><th>birthdate</th></tr>");
+            while (rs.next()) {
+                int id1 = rs.getInt("id");
+                String username1 = rs.getString("username");
+                String password1 = rs.getString("password");
+                String email1 = rs.getString("email");
+                String gender1 = rs.getString("gender");
+                Date birthdate1 = rs.getDate("birthdate");
+                out.println("<tr><td>"+id1+"</td>");
+                out.println("<td>"+username1+"</td>");
+                out.println("<td>"+password1+"</td>");
+                out.println("<td>"+email1+"</td>");
+                out.println("<td>"+gender1+"</td>");
+                out.println("<td>"+birthdate1+"</td>");
+            }
+            out.println("</tr></table></body><ml>");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
 
     }
 }

b27af5d5ecea1fb246b97d0b734cfea67cddd33c
2019211001000917XiongHoubing <77086458+2019211001000917XiongHoubing@users.noreply.github.com>
2021-06-15 21:36:34 +0800

    week15

--- src/main/java/com/XiongHoubing/controller/AdminOrderListServlet.java
+++ src/main/java/com/XiongHoubing/controller/AdminOrderListServlet.java
@@ -1,7 +1,8 @@
 package com.XiongHoubing.controller;
 
-import com.XiongHoubing.dao.ProductDao;
-import com.XiongHoubing.model.Product;
+import com.XiongHoubing.dao.OrderDao;
+import com.XiongHoubing.model.Order;
+import com.XiongHoubing.model.Payment;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,14 +11,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.List;
 
-@WebServlet(name = "ProductListServlet", value = "/admin/productList")
-public class ProductListServlet extends HttpServlet {
-    Connection con = null;
+@WebServlet(name = "AdminOrderListServlet", value = "/admin/orderList")
+public class AdminOrderListServlet extends HttpServlet {
+    private Connection con = null;
 
-    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
     public void init() throws ServletException {
         super.init();
         con = (Connection) getServletContext().getAttribute("con");
@@ -28,14 +31,12 @@ public class ProductListServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ProductDao productDao = new ProductDao();
-        try {
-            List<Product> productList = productDao.findAll(con);
-            request.setAttribute("productList", productList);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        String path = "/WEB-INF/views/admin/productList.jsp";
+        List<Payment> paymentTypeList = Payment.findAllPayment(con);
+        request.setAttribute("paymentTypeList",paymentTypeList);
+        OrderDao orderDao = new OrderDao();
+        List<Order> orderList = orderDao.findAll(con);
+        request.setAttribute("orderList",orderList);
+        String path = "/WEB-INF/views/admin/orderList.jsp";
         request.getRequestDispatcher(path).forward(request,response);
     }
 }
